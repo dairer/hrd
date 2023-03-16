4c824b2b3001494e757f7e278345acc06670343d
dairer <dairelhealy@gmail.com>
2023-03-16 17:41:49 +0000

    function to estimate GPD margins and HR copula jointly

--- R/bivariate_hr.R
+++ R/bivariate_hr.R
@@ -74,11 +74,11 @@ rhr = function(n, lambda){
 }
 
 calc_se = function(hess){
-  se = hess %>% solve() %>% diag %>% sqrt()
+  se = hess %>% solve(tol = 1e-1000) %>% diag %>% sqrt()
   1.96*se
 }
 
-hrc_ll = function(pars, u, v){
+hrc_ngll = function(pars, u, v){
   LL = -sum(dhr(u,v,lambda = pars, log = T))
   if(is.infinite(LL)) return(100^100)
   LL
@@ -113,7 +113,7 @@ fit_hrc = function(u, v, initial_est){
     stop("Uniform variables must be in the range [0,1]")
   }
 
-  this_fit = optim(fn=hrc_ll,
+  this_fit = optim(fn=hrc_ngll,
                    par = initial_est,
                    u = u,v = v,
                    method = 'Brent',
@@ -131,5 +131,93 @@ fit_hrc = function(u, v, initial_est){
     # return estimate and CI
     list(estimate = this_fit$par,
          ci = c(this_fit$par-this_se, this_fit$par+this_se))
+  }
+}
+
+# negative log likelihood of copula and margins
+hrc_gp_nll = function(pars, x, y){
+
+  if(any(pars[1] <= 0)) return(100^100)
+  if(pars[2]<0){
+    if(any(pars[1] > -1/pars[2])) return(100^100)
+    if(any((1+pars[2]*x/pars[1])< 0)) return(100^100)
+  }
+
+  if(any(pars[3] <= 0)) return(100^100)
+  if(pars[4]<0){
+    if(any(pars[3] > -1/pars[4])) return(100^100)
+    if(any((1+pars[4]*y/pars[3])< 0)) return(100^100)
+  }
+
+  if(pars[5]<=0 | pars[5]>10) return(100^100)
+
+  # likelihood of margin 1
+  llm1 = dgp(x, scale = pars[1], shape = pars[2])
+
+  # likelihood of margin 2
+  llm2 = dgp(y, scale = pars[3], shape = pars[4])
+
+  # likelihood of copula
+  unif_1 = pgp(x, scale = pars[1], shape = pars[2])
+  unif_2 = pgp(y, scale = pars[3], shape = pars[4])
+  llc = dhr(unif_1, unif_2, lambda = pars[5])
+
+  # full = likelihood
+  LL =  -sum(log(llm1*llm2*llc))
+
+  if(is.infinite(LL)) return(100^100)
+  if(is.na(LL)) return(100^100)
+  LL
+}
+
+#' Jointly fitting a bi-variate Hüsler-Reiss copula and generlaised Pareto margins
+#'
+#' @param x (numeric) margin 1, assumed to be generalised Pareto distributed
+#' @param y (numeric) margin 2, assumed to be generalised Pareto distributed
+#' @param initial_est (numeric) Vector of initial parameter estimates
+#'
+#' @return (list(estimate (numeric), ci (matrix))) List of 2 elements,
+#' first element is the estimates of the generalised Pareto distribution parameters and Hüsler Reiss copula parameter,
+#' second element is the estimated 95% confidence interval from the Hessian, the i-th row corresponds to the i-th estimate.
+#' @export
+#'
+#' @examples
+#' # sample from a Hüsler-Reiss copula
+#' copula_sample = rhr(1000, 2)
+#'
+#' # Transform margins to be GPD
+#' margin_1 = qgp(copula_sampe[,1], scale = 2, shape = -0.1)
+#' margin_2 = qgp(copula_sampe[,2], scale = 1.3, shape = -0.1)
+#'
+#' # fit margins and copula jointly
+#' fit_hrc_gp(margin_1, margin_2, initial_est = c(1,0.1,1,-0.12,1))
+fit_hrc_gp = function(x, y, initial_est){
+  # --- Error handling
+  # check variables are same length
+  if(length(x) != length(y)){
+    stop("Variables must be the same length")
+  }
+
+  # check if values are in the range [0,1]
+  unif_range_check = range(u,v)
+  if(any(unif_range_check < 0) | any(unif_range_check>1)){
+    stop("Variables must be in the range [0,1]")
+  }
+
+  this_fit = optim(fn=hrc_gp_nll,
+                   par = initial_est,
+                   x = x, y = y,
+                   hessian = T)
+
+  if(det(this_fit$hessian)==0){
+    cat("Could not compute confidence interval as det(hessian) = 0. Try different initial_est.")
+    list(Estimate = this_fit$par)
+  }else{
+
+    # estimate confidence interval from Hessian matric
+    this_se = calc_se(this_fit$hessian)
+    # return estimate and CI
+    list(estimate = this_fit$par,
+         ci = matrix(c(this_fit$par-this_se, this_fit$par+this_se), nrow = 5))
   }
 }
