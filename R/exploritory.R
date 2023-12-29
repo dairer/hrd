821df326a1845830ee177562bb8986726450d6c1
dáire <42574562+dairer@users.noreply.github.com>
2023-12-29 20:11:28 +0000

    Update exploritory.R
    Allowing user to specify order for variables to be plotted

--- R/exploritory.R
+++ R/exploritory.R
@@ -36,77 +36,158 @@ hrd_exploritory = function(dat, data_scale = T){
 #' This function plots the estimated dependence matrix of the Hüsler-Reiss copula where data is on any scale. The function transforms the data to uniform margins using an empirical ranking.
 #'
 #' @param dat (data.frame or matrix) Each column corresponds to a different site/variable, with component-wise maxima in each row.
+#' @param ord (vector) Vector describing the order for variables to be plotted.
 #' @param lower_diag If TRUE, the default, plot lower diagonal elements of dependence matrix
 #'
 #' @return (ggplot2::ggplot) Plotted dependence matrix
 #' @export
-explore_lambda = function(dat, lower_diag = T){
-
+explore_lambda = function(dat, ord = NULL, lower_diag = T){
   s1=s2=lambda=NULL
 
   all_pairs = hrd_exploritory(dat)
 
-  if(!lower_diag){
-    all_pairs = rbind(
-      all_pairs,
-      all_pairs %>%
-        dplyr::rename(s1 = s2,
-               s2 = s1) %>%
-        dplyr::select(s1, s2, lambda))
+  all_pairs = rbind(all_pairs %>%
+                      dplyr::select(s1, s2, lambda),
+                    all_pairs %>%
+                      rename(s1 = s2, s2 = s1) %>%
+                      dplyr::select(s1, s2, lambda))
+
+  if(!is.null(ord)){
+    all_pairs$s1 = factor(all_pairs$s1, levels = (ord))
+    all_pairs$s2 = factor(all_pairs$s2, levels = rev(ord))
+  }else{
+    all_pairs$s1 = factor(all_pairs$s1, levels = unique(sort(all_pairs$s1)))
+    all_pairs$s2 = factor(all_pairs$s2, levels = rev(unique(sort(all_pairs$s1))))
   }
 
-  all_pairs$s1 = base::factor(all_pairs$s1, levels = base::unique(all_pairs$s1) %>% sort)
-  all_pairs$s2 = base::factor(all_pairs$s2, levels = base::unique(all_pairs$s2) %>% sort %>% rev)
 
+  if(lower_diag == T){
+    all_pairs = all_pairs %>%
+      arrange(s1) %>%
+      filter(!duplicated(purrr::map2_chr(s1, s2, ~ paste(sort(c(.x, .y)), collapse = ""))))
+  }
 
-  all_pairs%>%
-    ggplot2::ggplot()+
-    ggplot2::geom_tile(ggplot2::aes(s1, s2, fill = lambda))+
+  plt = all_pairs %>%
+    arrange(s1) %>%
+    ggplot()+
+    geom_tile(aes(s1, s2, fill = lambda))+
     ggplot2::labs(fill = expression(lambda))+
     ggplot2::theme_minimal(12)+
-    ggplot2::scale_fill_gradientn(colours = RColorBrewer::brewer.pal(10,"Spectral"))+
     ggplot2::theme(axis.title = ggplot2::element_blank(),
                    axis.text.x = ggplot2::element_text(angle = 90,
                                                        vjust=0.5, hjust=1))
 
+
+  if(length(lims) == 1){
+    plt + ggplot2::scale_fill_gradientn(colours = RColorBrewer::brewer.pal(10,"Spectral"))
+  }else{
+    plt + ggplot2::scale_fill_gradientn(colours = RColorBrewer::brewer.pal(10,"Spectral"), limits = lims)
+  }
+
+  # s1=s2=lambda=NULL
+  #
+  # all_pairs = hrd_exploritory(dat)
+  #
+  # if(!lower_diag){
+  #   all_pairs = rbind(
+  #     all_pairs,
+  #     all_pairs %>%
+  #       dplyr::rename(s1 = s2,
+  #                     s2 = s1) %>%
+  #       dplyr::select(s1, s2, lambda))
+  # }
+  #
+  # all_pairs$s1 = base::factor(all_pairs$s1, levels = base::unique(all_pairs$s1) %>% sort)
+  # all_pairs$s2 = base::factor(all_pairs$s2, levels = base::unique(all_pairs$s2) %>% sort %>% rev)
+  #
+  #
+  # all_pairs%>%
+  #   ggplot2::ggplot()+
+  #   ggplot2::geom_tile(ggplot2::aes(s1, s2, fill = lambda))+
+  #   ggplot2::labs(fill = expression(lambda))+
+  #   ggplot2::theme_minimal(12)+
+  #   ggplot2::scale_fill_gradientn(colours = RColorBrewer::brewer.pal(10,"Spectral"))+
+  #   ggplot2::theme(axis.title = ggplot2::element_blank(),
+  #                  axis.text.x = ggplot2::element_text(angle = 90,
+  #                                                      vjust=0.5, hjust=1))
+
 }
+
+
+
 
 #' Estimate and plot matrix pairwise dependence measure
 #'
 #' This function plots the estimated extremal coefficient (chi) matrix derived from an estimated dependence matrix of the Hüsler-Reiss copula where data is on any scale. The function transforms the data to uniform margins using an empirical ranking.
 #'
 #' @param dat (data.frame or matrix) Each column corresponds to a different site/variable, with component-wise maxima in each row.
+#' @param ord (vector) Vector describing the order for variables to be plotted.
 #' @param lower_diag If TRUE, the default, plot lower diagonal elements of extremal coefficient (chi) matrix
 #' @param lims (numeric) Vector of length 2 controlling the limits of the scale for plotting extremal coefficient. Default plot has limit c(0,1).
 #'
 #' @return (ggplot2::ggplot) Plotted extremal coefficient (chi) matrix
 #' @export
-explore_chi = function(dat, lower_diag = T, lims = NA){
+explore_chi = function(dat, ord = NULL, lower_diag = T, lims = NA){
   s1=s2=lambda=chi=NULL
+
+  # all_pairs = hrd_exploritory(dat)
+  #
+  # if(!lower_diag){
+  #   all_pairs = rbind(
+  #     all_pairs,
+  #     all_pairs %>%
+  #       dplyr::rename(s1 = s2,
+  #                     s2 = s1) %>%
+  #       dplyr::select(s1, s2, lambda, chi))
+  # }
+  #
+  # all_pairs$s1 = base::factor(all_pairs$s1, levels = base::unique(all_pairs$s1) %>% sort)
+  # all_pairs$s2 = base::factor(all_pairs$s2, levels = base::unique(all_pairs$s2) %>% sort %>% rev)
+  #
+  #
+  # plt = all_pairs%>%
+  #   ggplot2::ggplot()+
+  #   ggplot2::geom_tile(ggplot2::aes(s1, s2, fill = chi))+
+  #   ggplot2::labs(fill = expression(chi))+
+  #   ggplot2::theme_minimal(12)+
+  #   ggplot2::theme(axis.title = ggplot2::element_blank(),
+  #                  axis.text.x = ggplot2::element_text(angle = 90,
+  #                                                      vjust=0.5, hjust=1))
+
 
   all_pairs = hrd_exploritory(dat)
 
-  if(!lower_diag){
-    all_pairs = rbind(
-      all_pairs,
-      all_pairs %>%
-        dplyr::rename(s1 = s2,
-               s2 = s1) %>%
-        dplyr::select(s1, s2, lambda, chi))
+  all_pairs = rbind(all_pairs %>%
+                      dplyr::select(s1, s2, chi),
+                    all_pairs %>%
+                      rename(s1 = s2, s2 = s1) %>%
+                      dplyr::select(s1, s2, chi))
+
+  if(!is.null(ord)){
+    all_pairs$s1 = factor(all_pairs$s1, levels = (ord))
+    all_pairs$s2 = factor(all_pairs$s2, levels = rev(ord))
+  }else{
+    all_pairs$s1 = factor(all_pairs$s1, levels = unique(sort(all_pairs$s1)))
+    all_pairs$s2 = factor(all_pairs$s2, levels = rev(unique(sort(all_pairs$s1))))
   }
 
-  all_pairs$s1 = base::factor(all_pairs$s1, levels = base::unique(all_pairs$s1) %>% sort)
-  all_pairs$s2 = base::factor(all_pairs$s2, levels = base::unique(all_pairs$s2) %>% sort %>% rev)
 
+  if(lower_diag == T){
+    all_pairs = all_pairs %>%
+      arrange(s1) %>%
+      filter(!duplicated(purrr::map2_chr(s1, s2, ~ paste(sort(c(.x, .y)), collapse = ""))))
+  }
 
-  plt = all_pairs%>%
-    ggplot2::ggplot()+
-    ggplot2::geom_tile(ggplot2::aes(s1, s2, fill = chi))+
-    ggplot2::labs(fill = expression(chi))+
-    ggplot2::theme_minimal(12)+
-    ggplot2::theme(axis.title = ggplot2::element_blank(),
-                   axis.text.x = ggplot2::element_text(angle = 90,
-                                                       vjust=0.5, hjust=1))
+  plt = all_pairs %>%
+    arrange(s1) %>%
+    ggplot()+
+    geom_tile(aes(s1, s2, fill = chi))+
+    labs(fill = expression(chi))+
+    theme_minimal(12)+
+    theme(axis.title = element_blank(),
+          axis.text.x = element_text(angle = 90,
+                                     vjust=0.5, hjust=1))
+
 
   if(length(lims) == 1){
     plt + ggplot2::scale_fill_gradientn(colours = RColorBrewer::brewer.pal(10,"Spectral"))
@@ -124,7 +205,7 @@ explore_chi = function(dat, lower_diag = T, lims = NA){
 #' @param lower_diag If TRUE, the default, plot lower diagonal elements of extremal coefficient (chi) matrix
 #' @param data_scale If TRUE, the default, function transforms the data to uniform margins using an empirical ranking.
 #' @NoRd
-emp_chi = function(dat, u, data_scale = T, lower_diag = T){
+emp_chi = function(dat, u, data_scale = T){
   s1=s2=lambda=NULL
   if(data_scale){
     get_emp_rank = function(x) rank(x)/(length(x)+1)
@@ -148,19 +229,19 @@ emp_chi = function(dat, u, data_scale = T, lower_diag = T){
 
   base::names(all_pairs) = c("s1", "s2", "emp_chi")
 
-  if(!lower_diag){
-    all_pairs = rbind(
-      all_pairs,
-      all_pairs %>%
-        dplyr::rename(s1 = s2,
-               s2 = s1) %>%
-        dplyr::select(s1, s2, lambda))
-  }
-
-  all_pairs$s1 = base::factor(all_pairs$s1, levels = base::unique(all_pairs$s1) %>% sort)
-  all_pairs$s2 = base::factor(all_pairs$s2, levels = base::unique(all_pairs$s2) %>% sort %>% rev)
-
-
+  # if(!lower_diag){
+  #   all_pairs = rbind(
+  #     all_pairs,
+  #     all_pairs %>%
+  #       dplyr::rename(s1 = s2,
+  #                     s2 = s1) %>%
+  #       dplyr::select(s1, s2, lambda))
+  # }
+  #
+  # all_pairs$s1 = base::factor(all_pairs$s1, levels = base::unique(all_pairs$s1) %>% sort)
+  # all_pairs$s2 = base::factor(all_pairs$s2, levels = base::unique(all_pairs$s2) %>% sort %>% rev)
+  #
+  #
 
   all_pairs %>%
     dplyr::as_tibble() %>%
@@ -173,30 +254,62 @@ emp_chi = function(dat, u, data_scale = T, lower_diag = T){
 #'
 #' @param dat (data.frame or matrix) Each column corresponds to a different site/variable, each row is an observation.
 #' @param u (numeric) Quantile at which to estimate extremal dependence coefficient
+#' @param ord (vector) Vector describing the order for variables to be plotted.
 #' @param data_scale If TRUE, the default, function transforms the data to uniform margins using an empirical ranking.
 #' @param lower_diag If TRUE, the default, plot lower diagonal elements of extremal coefficient (chi) matrix
 #' @param lims (numeric) Vector of length 2 controlling the limits of the scale for plotting extremal coefficient. Default plot has limit c(0,1).
 #'
 #' @return (ggplot2::ggplot) Plotted extremal coefficient (chi) matrix
 #' @export
-explore_emp_chi = function(dat, u = 0.8, data_scale = T, lower_diag = T, lims = NA){
+explore_emp_chi = function(dat, u = 0.8, ord = NULL, data_scale = T, lower_diag = T, lims = NA){
   s1=s2=NULL
-  plt = emp_chi(dat, u, data_scale = T) %>%
+  # plt = emp_chi(dat, u, data_scale = T) %>%
+  #   ggplot2::ggplot()+
+  #   ggplot2::geom_tile(ggplot2::aes(s1, s2, fill = emp_chi))+
+  #   ggplot2::labs(fill = expression(emp_chi))+
+  #   ggplot2::theme_minimal(12)+
+  #   ggplot2::labs(fill = expression(chi[u]))+
+  #   ggplot2::theme(axis.title = ggplot2::element_blank(),
+  #                  axis.text.x = ggplot2::element_text(angle = 90,
+  #                                                      vjust=0.5, hjust=1))
+  #
+
+  all_pairs = emp_chi(dat, u, data_scale = T)
+
+  all_pairs = rbind(all_pairs %>%
+                      dplyr::select(s1, s2, emp_chi),
+                    all_pairs %>%
+                      rename(s1 = s2, s2 = s1) %>%
+                      dplyr::select(s1, s2, emp_chi))
+
+  if(!is.null(ord)){
+    all_pairs$s1 = factor(all_pairs$s1, levels = (ord))
+    all_pairs$s2 = factor(all_pairs$s2, levels = rev(ord))
+  }else{
+    all_pairs$s1 = factor(all_pairs$s1, levels = unique(sort(all_pairs$s1)))
+    all_pairs$s2 = factor(all_pairs$s2, levels = rev(unique(sort(all_pairs$s1))))
+  }
+
+
+  if(lower_diag == T){
+    all_pairs = all_pairs %>%
+      arrange(s1) %>%
+      filter(!duplicated(purrr::map2_chr(s1, s2, ~ paste(sort(c(.x, .y)), collapse = ""))))
+  }
+
+  plt = all_pairs %>%
     ggplot2::ggplot()+
     ggplot2::geom_tile(ggplot2::aes(s1, s2, fill = emp_chi))+
     ggplot2::labs(fill = expression(emp_chi))+
     ggplot2::theme_minimal(12)+
     ggplot2::labs(fill = expression(chi[u]))+
     ggplot2::theme(axis.title = ggplot2::element_blank(),
-          axis.text.x = ggplot2::element_text(angle = 90,
-                                     vjust=0.5, hjust=1))
+                   axis.text.x = ggplot2::element_text(angle = 90,
+                                                       vjust=0.5, hjust=1))
+
   if(length(lims) == 1){
     plt + ggplot2::scale_fill_gradientn(colours = RColorBrewer::brewer.pal(10,"Spectral"))
   }else{
     plt + ggplot2::scale_fill_gradientn(colours = RColorBrewer::brewer.pal(10,"Spectral"), limits = lims)
   }
 }
-
-
-
-
